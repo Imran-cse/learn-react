d30bafaf924111b523b44d6b57a17c598b510ce9
Imran Hossain <imran.cse.ku@gmail.com>
2020-07-08 15:13:45 +0600

    app.js updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Music from './components/Music';
+import AudioHome from './components/AudioHome';
 
 function App() {
   return (
-    <Music />
+    <AudioHome />
   );
 }
 
